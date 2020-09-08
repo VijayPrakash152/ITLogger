eebf1b78257826c95d949ecd4710ad3283197d43
vijay <singhsidd152@gmail.com>
2020-09-08 15:32:37 +0530

    UI Completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Fragment, useEffect } from "react";
-import "materialize-css/dist/css/materialize.min.css";
-import M from "materialize-css/dist/js/materialize.min";
 import Searchbar from "./Components/Layout/SearchBar";
 import Logs from "./Components/logs/Logs";
-import "./App.css";
 import AddBtn from "./Components/Layout/AddBtn";
 import AddLogModal from "./Components/logs/AddLogModal";
+import EditLogModal from "./Components/logs/EditLogoModal";
+import AddTechModal from "./Components/techs/AddTechModal";
+import M from "materialize-css/dist/js/materialize.min";
+import TechListModal from "./Components/techs/TechListModal";
+import "materialize-css/dist/css/materialize.min.css";
+import "./App.css";
 
 const App = () => {
   useEffect(() => {
@@ -18,6 +21,9 @@ const App = () => {
       <div className="container">
         <AddBtn />
         <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
+        <TechListModal />
         <Logs />
       </div>
     </Fragment>
--- src/Components/logs/EditLogoModal.js
+++ src/Components/logs/EditLogoModal.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import M from "materialize-css/dist/js/materialize.min";
-const AddLogModal = () => {
+const EditLogModal = () => {
   const [message, setMessage] = useState("");
   const [attention, setAttention] = useState(false);
   const [tech, setTech] = useState("");
@@ -9,13 +9,14 @@ const AddLogModal = () => {
       M.toast({ html: "Please enter a message and tech" });
     else {
       console.log(message, tech, attention);
+      // Clear Fields
       setMessage("");
       setTech("");
       setAttention(false);
     }
   };
   return (
-    <div id="add-log-modal" className="modal" style={modalStyle}>
+    <div id="edit-log-modal" className="modal" style={modalStyle}>
       <div className="modal-content">
         <h4>Enter System Log</h4>
         <div className="row">
@@ -81,4 +82,4 @@ const modalStyle = {
   width: "75%",
   height: "75%",
 };
-export default AddLogModal;
+export default EditLogModal;
